1a92595f850dbd617eb7e41e36b114c6088fb451
Mkabbas <mohdkazimabbas1@gmail.com>
2022-09-09 22:49:25 +0530

    Retrieve input in server

--- app.js
+++ app.js
@@ -10,8 +10,16 @@ app.use(express.static("public"));
 
 app.listen(3000, function() {
     console.log("Server is running at Port 3000.");
-})
+});
 
 app.get("/", function(req, res) {
     res.sendFile(__dirname + "/signup.html");
-})
+});
+
+app.post("/", function(req, res) {
+    var firstName = req.body.fname;
+    var lastName = req.body.lname;
+    var email = req.body.emailid;
+    
+
+});
