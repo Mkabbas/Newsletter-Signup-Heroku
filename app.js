63684a135c93e0ce615a1385098abb1db34f00a7
Mkabbas <mohdkazimabbas1@gmail.com>
2022-09-10 17:19:32 +0530

    Connection established with MailChimp Servers. Data entry into MailChimp server successful.Project completed

--- app.js
+++ app.js
@@ -20,6 +20,42 @@ app.post("/", function(req, res) {
     var firstName = req.body.fname;
     var lastName = req.body.lname;
     var email = req.body.emailid;
+
+    var data = {
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+            }
+        ]
+    }
+
+    var options = {
+        url: "https://us11.api.mailchimp.com/3.0/lists/714a8b061d",
+        method: "post",
+        headers: {
+            Authorization: "Kazim a965117e6c7353c634d45eec8b11a232-us11"
+        },
+        body: JSON.stringify(data)
+    }
     
+    request(options, function(error, response, body) {
+        if (response.statusCode == 200) {
+            console.log("success");
+            res.sendFile(__dirname + "/success.html");
+        }
+        else {
+            console.log("failure");
+            res.sendFile(__dirname + "/failure.html");
+        }
+    })
+
 
 });
+
+//MailChimp API Key - a965117e6c7353c634d45eec8b11a232-us11
+//List ID - 714a8b061d
